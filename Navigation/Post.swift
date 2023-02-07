d1d032167431c77b3156dce32284c93193dc7059
olmarts <oleg_oyun@mail.ru>
2023-02-07 20:40:22 +0700

    ready for a Pull Requst

---
+++ Navigation/Post.swift
@@ -0,0 +1,14 @@
+//
+//  Post.swift
+//  Navigation
+//
+//  Created by user1 on 06.02.2023.
+//
+
+import Foundation
+
+// 7.1. Создайте структуру Post со свойством title: String.
+struct Post {
+    var title: String
+}
+
